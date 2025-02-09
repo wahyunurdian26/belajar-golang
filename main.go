e989f8debb2a01871473d34210d3e49161ea7e49
wahyunurdian26 <wahyunurdian98@gmail.com>
2025-02-09 15:57:03 +0700

    tambah main

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const AppName = "Online CSS"
+
+var status bool = true
+var progress int = 30
+
+func main() {
+	fmt.Println("Nama Apllikasi: ", AppName)
+	fmt.Println("Status:", status)
+	fmt.Println("Progress:", progress)
+
+
+}
