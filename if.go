5383d78b3cb80df0b999f2807484742d36975adf
wahyunurdian26 <wahyunurdian98@gmail.com>
2025-02-10 08:36:57 +0700

    if else

---
+++ if.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "pupung"
+
+	if name == "lowi" {
+		fmt.Println(name)
+	} else if name == "dino" {
+		fmt.Println("dino")
+	} else if name == "pupung" {
+		fmt.Println("pupung")
+		}else {
+		fmt.Println("bukan")
+	}
+
+	
+// soft statment
+	if kucing := len("kuc");kucing >= 5 {
+		fmt.Println("nama sesuai")
+	} else if 
+	kucing < 5{
+		fmt.Println("nama terlalu pendek")
+	} 
+	
+	
+}
